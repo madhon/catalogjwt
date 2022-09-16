f87d37797629ca12d80f7e25177cd719ddd0dd9b
Madhon <madhon@madhon.co.uk>
2022-09-16 12:46:49 +0100

    feat: more validators and sonar update

--- Catalog.Auth/ViewModel/LoginModel.cs
+++ Catalog.Auth/ViewModel/LoginModel.cs
@@ -1,14 +1,8 @@
 ﻿namespace Catalog.Auth.ViewModel
 {
-    using System.ComponentModel.DataAnnotations;
-
     public class LoginModel
     {
-        [Required(ErrorMessage = "Email address is required")]
-        [DataType(DataType.EmailAddress)]
         public string Email { get; set; } = string.Empty;
-
-        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; } = string.Empty;
     }
 }
