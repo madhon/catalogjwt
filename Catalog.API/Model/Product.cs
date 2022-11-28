daf86331952b5796562f40cd0c01a351d310eec6
Jonathon Douglas <jdouglas@acquisinsurance.com>
2022-11-28 16:58:35 +0000

    fix: brand null ref fix

--- Catalog.API/Model/Product.cs
+++ Catalog.API/Model/Product.cs
@@ -8,6 +8,6 @@
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public string PictureUri { get; set; } = string.Empty;
-        public Brand Brand { get; set; } = new ();
+        public Brand? Brand { get; set; } = new ();
     }
 }
