834a277494784d8ac9c86d424a801ff6f12389cf
Madhon <madhon@madhon.co.uk>
2023-12-18 13:20:39 +0000

    feat: Add Aspire AppHost

---
+++ Catalog.AppHost/Program.cs
@@ -0,0 +1,17 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var authDb = builder.AddSqlServerConnection("AuthDb", "Server=.;Database=AuthDb;Trusted_Connection=True;");
+
+var authService = builder.AddProject<Projects.Catalog_Auth>("auth")
+    .WithReference(authDb);
+
+var catalogDb = builder.AddSqlServerConnection("CatalogDb", "Server=.;Database=CatalogDb;Trusted_Connection=True;");
+
+var catalogService = builder.AddProject<Projects.Catalog_API_Web>("catalog")
+    .WithReference(catalogDb);
+
+builder.AddProject<Projects.Catalog_Gateway>("gatewat")
+    .WithReference(authService)
+    .WithReference(catalogService);
+
+builder.Build().Run();
