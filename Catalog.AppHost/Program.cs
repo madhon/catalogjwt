95f5c444188afe76de42574c9d8b7ccdeaa6fd41
Madhon <madhon@madhon.co.uk>
2023-12-21 13:46:16 +0000

    build(deps): Bump Aspire to preview 2

--- Catalog.AppHost/Program.cs
+++ Catalog.AppHost/Program.cs
@@ -1,14 +1,8 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
-var authDb = builder.AddSqlServerConnection("AuthDb", "Server=.;Database=AuthDb;Trusted_Connection=True;");
-
-var authService = builder.AddProject<Projects.Catalog_Auth>("auth")
-    .WithReference(authDb);
-
-var catalogDb = builder.AddSqlServerConnection("CatalogDb", "Server=.;Database=CatalogDb;Trusted_Connection=True;");
+var authService = builder.AddProject<Projects.Catalog_Auth>("auth");
 
 var catalogService = builder.AddProject<Projects.Catalog_API_Web>("catalog")
-    .WithReference(catalogDb)
     .WithReference(authService);
 
 builder.AddProject<Projects.Catalog_Gateway>("gateway")
