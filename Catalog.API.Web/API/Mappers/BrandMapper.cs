95aef0b010fbc8065934a16713469ad23bf822ae
Madhon <madhon@madhon.co.uk>
2024-11-18 11:20:24 +0000

    feat: bump packages and move to .net 9.0

--- Catalog.API.Web/API/Mappers/BrandMapper.cs
+++ Catalog.API.Web/API/Mappers/BrandMapper.cs
@@ -5,7 +5,9 @@ using Catalog.API.Web.API.Endpoints.Requests;
 [Mapper]
 public partial class BrandMapper
 {
+    [MapperIgnoreTarget(nameof(Brand.Id))]
     public partial Brand MapAddBrandRequestToBrand(AddBrandRequest request);
 
+    [MapperIgnoreSource(nameof(Brand.Id))]
     public partial AddBrandRequest MapBrandAddBrandRequest(Brand request);
 }
--- Catalog.API.Web/API/Mappers/ProductMapper.cs
+++ Catalog.API.Web/API/Mappers/ProductMapper.cs
@@ -5,5 +5,7 @@ using Catalog.API.Web.API.Endpoints.Requests;
 [Mapper]
 public partial class ProductMapper
 {
+    [MapperIgnoreTarget(nameof(Product.Id))]
+    [MapperIgnoreTarget(nameof(Product.Brand))]
     public partial Product MapAddProductRequestToProduct(AddProductRequest request);
 }
