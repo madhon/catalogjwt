28026b8a189f03f3c937c567d07a4f8c04b019f7
Madhon <madhon@madhon.co.uk>
2025-09-26 11:42:48 +0100

    fix: remove SetDbStatementForText as its been removed from Otel SDK

--- Catalog.ServiceDefaults/ServiceDefaultsExtensions.cs
+++ Catalog.ServiceDefaults/ServiceDefaultsExtensions.cs
@@ -93,7 +93,6 @@ public static partial class ServiceDefaultsExtensions
                     .AddSqlClientInstrumentation(sci =>
                     {
                         sci.RecordException = true;
-                        sci.SetDbStatementForText = true;
                     });
             });
 
